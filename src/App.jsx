17811172704643c95a20f4d16cb5aabbde15694c
Yatendra42 <yatendrajain2014@gmail.com>
2025-10-16 00:14:39 +0530

    flag path updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import SearchBar from "./components/SerachBar/SearchBar";
-import Flags from "./components/Flags/Flags"
+import Flags from "./components/Flags/Flags.jsx";
 
 
 import "./App.scss";
